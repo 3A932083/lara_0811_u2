12f61a42cdaf31a972f1d7ae3d43a63df71781cf
jiang shu yi <3a932083@gm.student.ncut.edu.tw>
2022-11-02 03:52:24 +0800

    新增route

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,10 @@ Route::get('r2', function() {
 Route::get('r1', function() {
     return redirect('r2');
 });
+Route::get('r3', function() {
+    return redirect('r2');
+});
+
 
 Route::get('hello/{name?}', function($name='Everybody') {
     return 'Hello, '.$name;
